7f23d3f32c9da7c27790072cb7b66c1c42a65b06
JrnJ <jeroenjochems@outlook.com>
2025-02-27 12:06:45 +0100

    project

---
+++ LiveMapDashboard.Tests/DummyTest.cs
@@ -0,0 +1,18 @@
+﻿namespace LiveMapDashboard.Tests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            // Arrange
+            int index = 0;
+
+            // Act
+            index += 2;
+
+            // Assert
+            Assert.Equal(2, index);
+        }
+    }
+}
