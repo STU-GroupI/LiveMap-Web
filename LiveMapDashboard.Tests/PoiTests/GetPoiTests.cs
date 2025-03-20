2d05244abf138b26b6c66f239f6d5547264f30c5
ThomasRuijters <83541740+ThomasRuijters@users.noreply.github.com>
2025-03-20 22:03:27 +0100

    Add shouldly to make tests more readable (fluent validations is payed now, so lets not use them). Rename tests to be more expressive.

--- LiveMapDashboard.Tests/PoiTests/GetPoiTests.cs
+++ LiveMapDashboard.Tests/PoiTests/GetPoiTests.cs
@@ -3,6 +3,7 @@ using LiveMap.Application.PointOfInterest.Requests;
 using LiveMap.Application.PointOfInterest.Responses;
 using LiveMap.Domain.Models;
 using Microsoft.OpenApi.Models;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,152 +15,124 @@ public class GetPoiTests
 {
     [Theory]
     [MemberData(nameof(GetValidPointOfInterestIds))]
-    public async Task Doing_ReadSingle_Given_ValidId_When_PoIExists_ResultsTo_ResponseWithPoI(
-        Guid id,
-        StubPointOfInterestRepository repository)
+    public async Task ReadSingle_WithValidId_WhenPoIExists_ShouldReturnResponseWithPoI(
+    Guid id,
+    StubPointOfInterestRepository repository)
     {
         // Arrange
         var expectedPoi = repository.pois[0];
-        var expectedResponse = new GetSingleResponse(expectedPoi);
-
-        var guid = expectedPoi.Id;
-        var request = new GetSingleRequest(guid);
+        var request = new GetSingleRequest(expectedPoi.Id);
         var handler = new GetSingleHandler(repository);
 
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Same(actualResult.PointOfInterest, expectedPoi);
+        actualResult.PointOfInterest.ShouldBe(expectedPoi);
     }
 
     [Theory]
     [InlineData("00000000-0000-0000-0000-000000000001")]
-    public async Task Doing_ReadSingle_Given_ValidId_When_PoINotExists_ResultsTo_ResponseWithoutPoI(Guid nonExistentId)
+    public async Task ReadSingle_WithValidId_WhenPoIDoesNotExist_ShouldReturnResponseWithoutPoI(Guid nonExistentId)
     {
         // Arrange
-        PointOfInterest? expectedPoi = null;
-        var expectedResponse = new GetSingleResponse(expectedPoi);
-
         var request = new GetSingleRequest(nonExistentId);
-
-        var handler = new GetSingleHandler(
-            new StubPointOfInterestRepository(
-                new List<Map>()));
+        var handler = new GetSingleHandler(new StubPointOfInterestRepository(new List<Map>()));
 
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Null(actualResult.PointOfInterest);
+        actualResult.PointOfInterest.ShouldBeNull();
     }
 
     [Theory]
     [MemberData(nameof(GetValidMapIds))]
-    public async Task Doing_ReadMultiple_Given_ValidMapId_When_MapExists_And_MapHasPois_ResultsTo_ExpectedPoisForMap(
+    public async Task ReadMultiple_WithValidMapId_WhenMapExistsAndHasPois_ShouldReturnExpectedPoisForMap(
         Guid id,
         StubPointOfInterestRepository repository)
     {
         // Arrange
         var map = repository.maps[0];
-        var expectedResponse = new GetMultipleResponse(map.PointOfInterests);
-
-        var guid = map.Id;
-        var request = new GetMultipleRequest(guid, null, null);
+        var request = new GetMultipleRequest(map.Id, null, null);
         var handler = new GetMultipleHandler(repository);
 
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Equal(expectedResponse.PointsOfInterests.Count(), actualResult.PointsOfInterests.Count());
+        actualResult.PointsOfInterests.Count().ShouldBe(map.PointOfInterests.Count());
     }
-
 
     [Theory]
     [MemberData(nameof(GetValidMapIdsWithSkipWithoutTake))]
-    public async Task Doing_ReadMultiple_Given_ValidMapId_With_ValidSkip_When_MapExists_And_MapHasPois_ResultsTo_ExpectedPoisFromMap(
+    public async Task ReadMultiple_WithValidMapId_AndValidSkip_WhenMapExistsAndHasPois_ShouldReturnExpectedPoisFromMap(
         Map map,
         int skip,
         StubPointOfInterestRepository repository)
     {
         // Arrange
-        var expectedResponse = new GetMultipleResponse(map.PointOfInterests
-            .Skip(skip)
-            .ToList());
-
-        var guid = map.Id;
-        var request = new GetMultipleRequest(guid, skip, null);
+        var expectedPois = map.PointOfInterests.Skip(skip).ToList();
+        var request = new GetMultipleRequest(map.Id, skip, null);
         var handler = new GetMultipleHandler(repository);
 
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Equal(expectedResponse.PointsOfInterests.Count(), actualResult.PointsOfInterests.Count());
+        actualResult.PointsOfInterests.ShouldBe(expectedPois);
     }
 
     [Theory]
     [MemberData(nameof(GetValidMapIdsWithSkipAndTake))]
-    public async Task Doing_ReadMultiple_Given_ValidMapId_With_ValidSkip_And_ValidTake_When_MapExists_And_MapHasPois_ResultsTo_ExpectedPoisForMap(
+    public async Task ReadMultiple_WithValidMapId_AndValidSkipAndTake_WhenMapExistsAndHasPois_ShouldReturnExpectedPoisForMap(
         Map map,
         int skip,
         int take,
         StubPointOfInterestRepository repository)
     {
         // Arrange
-        var expectedResponse = new GetMultipleResponse(map.PointOfInterests
-            .Skip(skip)
-            .Take(take)
-            .ToList());
-
-        var guid = map.Id;
-        var request = new GetMultipleRequest(guid, skip, take);
+        var expectedPois = map.PointOfInterests.Skip(skip).Take(take).ToList();
+        var request = new GetMultipleRequest(map.Id, skip, take);
         var handler = new GetMultipleHandler(repository);
 
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Equal(expectedResponse.PointsOfInterests.Count(), actualResult.PointsOfInterests.Count());
+        actualResult.PointsOfInterests.ShouldBe(expectedPois);
     }
 
     [Theory]
     [InlineData("00000000-0000-0000-0000-000000000002")] // Non-existing Map ID
-    public async Task Doing_ReadMultiple_Given_InvalidMapId_ResultsTo_EmptyResponse(Guid invalidMapId)
+    public async Task ReadMultiple_WithInvalidMapId_ShouldReturnEmptyResponse(Guid invalidMapId)
     {
         // Arrange
-        var expectedResponse = new GetMultipleResponse([]);
+        var request = new GetMultipleRequest(invalidMapId, null, null);
+        var handler = new GetMultipleHandler(new StubPointOfInterestRepository(new List<Map>()));
 
-        var guid = Guid.NewGuid();
-        var request = new GetMultipleRequest(guid, null, null);
-        var handler = new GetMultipleHandler(
-            new StubPointOfInterestRepository(
-                new List<Map>()));
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Equal(expectedResponse.PointsOfInterests.Count(), actualResult.PointsOfInterests.Count());
+        actualResult.PointsOfInterests.ShouldBeEmpty();
     }
 
     [Theory]
     [MemberData(nameof(GetValidMapsWithoutPois))]
-    public async Task Doing_ReadMultiple_Given_ValidMapId_When_MapHasNoPois_ResultsTo_EmptyResponse(
+    public async Task ReadMultiple_WithValidMapId_WhenMapHasNoPois_ShouldReturnEmptyResponse(
         Map map,
         StubPointOfInterestRepository repository)
     {
         // Arrange
-        var expectedResponse = new GetMultipleResponse([]);
-        var guid = Guid.NewGuid();
-        var request = new GetMultipleRequest(guid, null, null);
+        var request = new GetMultipleRequest(map.Id, null, null);
         var handler = new GetMultipleHandler(repository);
 
         // Act
         var actualResult = await handler.Handle(request);
 
         // Assert
-        Assert.Equal(expectedResponse.PointsOfInterests.Count(), actualResult.PointsOfInterests.Count());
+        actualResult.PointsOfInterests.ShouldBeEmpty();
     }
 
     public static IEnumerable<object[]> GetValidPointOfInterestIds()
