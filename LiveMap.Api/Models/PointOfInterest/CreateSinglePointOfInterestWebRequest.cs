a49ac945919632ea7f1ccd9156d436aa14de76c4
JoshavEngelen <josha@mixnmac.nl>
2025-04-07 21:18:27 +0200

    feat: building post request for poi

---
+++ LiveMap.Api/Models/PointOfInterest/CreateSinglePointOfInterestWebRequest.cs
@@ -0,0 +1,11 @@
+﻿using LiveMap.Domain.Models;
+
+namespace LiveMap.Api.Models.SuggestedPoi;
+
+public record CreateSinglePoiSuggestionWebRequest(
+    string Title, 
+    string Description, 
+    string Category,
+    Guid MapId,
+    Coordinate Coordinate, 
+    bool isWheelchairAccessible);
