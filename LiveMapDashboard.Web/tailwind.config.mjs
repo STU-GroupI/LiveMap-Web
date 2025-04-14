f287e7896b9493d69df191b1212885c4c179c552
Luka <60887692+BotSquirrel@users.noreply.github.com>
2025-04-14 11:51:59 +0200

    Fix preline + dark toggle (#39)
    * Fix preline
    
    * add preline fix with dark theme

---
+++ LiveMapDashboard.Web/tailwind.config.mjs
@@ -0,0 +1,11 @@
+﻿import forms from '@tailwindcss/forms';
+
+export default {
+    content: ['./Views/**/*.cshtml', './ViewComponents/**/*.cshtml', './Pages/**/*.cshtml', './wwwroot/js/**/*.js'],
+    plugins: [
+        forms,
+    ],
+    theme: {
+        extend: {},
+    },
+};
