58e557ae5f8f999998a65846b7c2080e7a802b31
JrnJ <jeroenjochems@outlook.com>
2025-04-14 11:13:03 +0200

    voor luka

---
+++ LiveMapDashboard.Web/Controllers/SuggestionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiveMapDashboard.Web.Controllers
+{
+    public class SuggestionsController : Controller
+    {
+        private readonly ILogger<SuggestionsController> _logger;
+
+        public SuggestionsController(ILogger<SuggestionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Single()
+        {
+            return View();
+        }
+    }
+}
