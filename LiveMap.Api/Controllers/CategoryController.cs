00566f8e0ebb1dd09ad15ee2ca5d5d7747a98125
SuperGameLegend <e.sagir@student.avans.nl>
2025-04-17 14:40:57 +0200

    Update

--- LiveMap.Api/Controllers/CategoryController.cs
+++ LiveMap.Api/Controllers/CategoryController.cs
@@ -1,4 +1,4 @@
-using LiveMap.Api.Models.Category;
+﻿using LiveMap.Api.Models.Category;
 using LiveMap.Application;
 using LiveMap.Application.Category.Requests;
 using LiveMap.Application.Category.Responses;
@@ -15,7 +15,7 @@ public class CategoryController : ControllerBase
 
 
 
-        /// <summary>
+    /// <summary>
     /// Get a specific category.
     /// </summary>
     /// <param name="name">The id of the specified Category.</param>
@@ -42,7 +42,7 @@ public class CategoryController : ControllerBase
         return Ok(category);
     }
 
-        /// <summary>
+    /// <summary>
     /// Creates a new category
     /// </summary>
     /// <param name="category">The given category.</param>
@@ -57,12 +57,12 @@ public class CategoryController : ControllerBase
         [FromBody] CreateSingleCategoryWebRequest webRequest,
         [FromServices] IRequestHandler<CreateSingleRequest, CreateSingleResponse> handler)
     {
-        var poi = new Category()
+        var category = new Category()
         {
             CategoryName = webRequest.CategoryName,
         };
 
-        var request = new CreateSingleRequest(poi);
+        var request = new CreateSingleRequest(category);
 
         try
         {
@@ -97,6 +97,32 @@ public class CategoryController : ControllerBase
     }
 
     /// <summary>
+    /// Creates a new category
+    /// </summary>
+    /// <param name="category">The given category.</param>
+    /// <returns> The RFC with callback URL </returns>
+    /// <response code="201">Response with the created </response>
+    /// <response code="500">Something went very wrong</response>
+    [HttpPatch("{oldName}")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType<(string, Category)>(StatusCodes.Status201Created)]
+    [ProducesResponseType<(int, object)>(StatusCodes.Status500InternalServerError)]
+
+    public async Task<IActionResult> Patch(
+        [FromRoute] string oldName,
+        [FromBody] UpdateSingleCategoryWebRequest webRequest,
+        [FromServices] IRequestHandler<UpdateSingleRequest, UpdateSingleResponse> handler)
+    {
+        var request = new UpdateSingleRequest(oldName, webRequest.NewCategoryName);
+        var response = await handler.Handle(request);
+
+        if (response.Success)
+            return Ok(new { oldName = response.oldname, newName = response.newname });
+
+        return NotFound("Category not found.");
+    }
+
+    /// <summary>
     /// Deletes a category by name.
     /// </summary>
     /// <param name="name">Name of the category to delete.</param>
@@ -121,7 +147,7 @@ public class CategoryController : ControllerBase
 
             return NoContent();
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             return StatusCode(StatusCodes.Status500InternalServerError, ex.ToString());
         }
