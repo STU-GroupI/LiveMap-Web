4d3758ea46ab7bbbab2141619e95879389a7af31
ThomasRuijters <83541740+ThomasRuijters@users.noreply.github.com>
2025-04-18 20:40:34 +0200

    merge: Sync with main

---
+++ LiveMapDashboard.Web/Models/Suggestion/PoiSuggestionsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using LiveMap.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LiveMapDashboard.Web.Models.Suggestion;
+
+public sealed record PoiSuggestionsViewModel(
+    Guid MapId,
+    int? Skip,
+    int? Take,
+    bool? Ascending,
+    SuggestedPointOfInterest[] SuggestedPointOfInterests
+    ) : IValidatableObject
+{
+    public static PoiSuggestionsViewModel Empty =>
+        new(
+            MapId: Guid.Empty,
+            Skip: null,
+            Take: null,
+            Ascending: null,
+            SuggestedPointOfInterests: []
+            );
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        List<ValidationResult> results = [];
+        return results;
+    }
+
+    public static string ToDateTime(DateTime? dateTime)
+    {
+        if (dateTime == null)
+        {
+            return "Geen Datum";
+        }
+        else
+        {
+            return dateTime.Value.ToString("d MMMM yyyy HH:mm", new System.Globalization.CultureInfo("nl-nl"));
+        }
+    }
+}
