06a2fd82ae866c4a5ccbd0c4c2f25847752f5459
SuperGameLegend <e.sagir@student.avans.nl>
2025-04-18 16:53:26 +0200

    Necessary changes added

--- LiveMap.Application/Category/Responses/UpdateSingleResponse.cs
+++ LiveMap.Application/Category/Responses/UpdateSingleResponse.cs
@@ -1,4 +1,4 @@
 namespace LiveMap.Application.Category.Responses;
 using Domain.Models;
 
-public sealed record UpdateSingleResponse(string oldname, string newname, bool Success);
+public sealed record UpdateSingleResponse(string oldName, string newName, bool Success);
