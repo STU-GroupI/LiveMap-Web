04fc29b410d6c8f3e8e53ad1bcf606145272d6a6
Luka <60887692+BotSquirrel@users.noreply.github.com>
2025-04-21 18:56:51 +0200

    Integrate feature #20: delete POI in CRUD + styling fixes (#64)
    * Feat #20: Backend (#57)
    
    * feat: Delete Poi API backend ready
    
    * feat: Completed tests
    
    * feat: Pre-made Delete for http backend service
    
    * feat: Added a form of cascading to remove rfc's related to a poi
    
    * fix: Put the deletion of poi and possible rfc('s) in a transaction
    
    * feat: Removed unnecessary includes
    
    * fix: Removed return object and commented out tests for potential future use
    
    * fix: Removed if statement
    
    * fix: Changed uri for Delete request
    
    * fix: Changed response comment in PointOfInterestController Delete method
    
    * Bind frontend delete to backend + delete modal + go back modal (#62)
    
    * feat: bind frontend form and backend
    
    * feat: add delete and go back modals
    
    * fix: styling after merge and form logic
    
    * fix: Added bool as return type for delete method
    
    * fix: Implement proper error propegation in the API.
    
    * fix: "whitespace" 😉
    
    ---------
    
    Co-authored-by: JoshavEngelen <103433509+JoshavEngelen@users.noreply.github.com>
    Co-authored-by: JoshavEngelen <josha@mixnmac.nl>
    Co-authored-by: ThomasRuijters <83541740+ThomasRuijters@users.noreply.github.com>

--- LiveMap.Application/PointOfInterest/Persistance/IPointOfInterestRepository.cs
+++ LiveMap.Application/PointOfInterest/Persistance/IPointOfInterestRepository.cs
@@ -7,8 +7,10 @@ public interface IPointOfInterestRepository
     public Task<Models.PointOfInterest?> GetSingle(Guid id);
 
     public Task<ICollection<Models.PointOfInterest>> GetMultiple(Guid mapId, int? skip, int? take);
-
+    
     public Task<Models.PointOfInterest> Create(Models.PointOfInterest pointOfInterest);
 
     public Task<Models.PointOfInterest?> Update(Models.PointOfInterest pointOfInterest);
+    
+    public Task<bool> DeleteSingle(Guid id);
 }
--- LiveMapDashboard.Web/Controllers/PoiController.cs
+++ LiveMapDashboard.Web/Controllers/PoiController.cs
@@ -50,6 +50,42 @@ namespace LiveMapDashboard.Web.Controllers
             return View("PoiForm", viewModel);
         }
 
+        [HttpPost("delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(
+            [FromServices] IPointOfInterestService service,
+            [FromServices] IViewModelProvider<PoiCrudformViewModel> provider,
+            PoiCrudformViewModel viewModel)
+        {
+            var poiId = viewModel.Id is not null
+                ? Guid.Parse(viewModel.Id)
+                : Guid.Empty;
+            
+            if (poiId == Guid.Empty)
+            {
+                ViewData["ErrorMessage"] = "The point of interest was not found.";
+                return View("PoiForm", await provider.Provide());
+            }
+            
+            var result = await service.Delete(poiId);
+            
+            if (result.IsSuccess)
+            {
+                ViewData["SuccessMessage"] = "The point of interest and its contents where successfully deleted!";
+                return RedirectToAction(nameof(Index));
+            }
+            
+            ViewData["ErrorMessage"] = result.StatusCode switch
+            {
+                HttpStatusCode.BadRequest => "The submitted data was invalid. Please check the data you submitted.",
+                HttpStatusCode.Unauthorized => "You are not authorized to perform this action.",
+                HttpStatusCode.ServiceUnavailable => "The application unavailable. Please try again later.",
+                _ => "Something went wrong while trying to contact the application. Please try again later"
+            };
+            
+            return View("PoiForm", await provider.Provide());
+        }
+
         [HttpPost]
         [Route("")]
         [ValidateAntiForgeryToken]
@@ -65,11 +101,17 @@ namespace LiveMapDashboard.Web.Controllers
             }
 
             var poi = viewModel.ToDomainPointOfInterest();
+            var isNewPoi = poi.Id == Guid.Empty || poi.Id.ToString() == string.Empty;
 
-            var result = poi.Id == Guid.Empty || poi.Id.ToString() == string.Empty
+            var result = isNewPoi
                 ? await service.CreateSingle(poi)
                 : await service.UpdateSingle(poi);
 
+            if (isNewPoi)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            
             if (result.IsSuccess)
             {
                 ViewData["SuccessMessage"] = "Your request was successfully processed!";
@@ -84,7 +126,7 @@ namespace LiveMapDashboard.Web.Controllers
                     _ => "Something went wrong while trying to contact the application. Please try again later"
                 };
             }
-
+            
             return View("PoiForm", await provider.Hydrate(viewModel));
         }
     }
