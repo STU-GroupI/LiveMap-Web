ac868db3603f06902dc528dad9a62e460be33cbc
SuperGameLegend <e.sagir@student.avans.nl>
2025-04-23 14:23:46 +0200

    Added required fields to satisfy Roslyn

--- LiveMap.Domain/Models/Map.cs
+++ LiveMap.Domain/Models/Map.cs
@@ -2,9 +2,9 @@
 public class Map
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public Coordinate[] Area { get; set; }
-    public Coordinate Coordinate { get; set; }
+    public required string Name { get; set; }
+    public required Coordinate[] Area { get; set; }
+    public required Coordinate Coordinate { get; set; }
 
-    public virtual ICollection<PointOfInterest> PointOfInterests { get; set; }
+    public required virtual ICollection<PointOfInterest> PointOfInterests { get; set; }
 }
