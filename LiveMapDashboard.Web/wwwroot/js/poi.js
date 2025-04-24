ccf1d0a3ed5e74fe0ab08643adc2e1b5c0cccd6d
JrnJ <jeroenjochems@outlook.com>
2025-04-24 15:53:55 +0200

    fix

--- LiveMapDashboard.Web/wwwroot/js/poi.js
+++ LiveMapDashboard.Web/wwwroot/js/poi.js
@@ -1,6 +1,18 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
     const imageInput = document.querySelector('#image');
+    const previewImage = document.querySelector('#previewImage');
+    const previewImageError = document.querySelector('#imagePreviewError');
+
+    previewImageError.hidden = true;
+    previewImage.src = imageInput.value;
+
     imageInput.addEventListener('change', () => {
-        document.querySelector('#previewImage').src = imageInput.value;
+        previewImageError.hidden = true;
+        previewImage.src = imageInput.value;
     });
+
+    previewImage.onerror = () => {
+        previewImage.src = '';
+        previewImageError.hidden = false;
+    };
 });
