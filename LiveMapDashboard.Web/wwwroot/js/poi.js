306ccf8befcac70bcb96caecfef73535f7cb719f
JrnJ <jeroenjochems@outlook.com>
2025-04-24 15:29:29 +0200

    image preview

---
+++ LiveMapDashboard.Web/wwwroot/js/poi.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const imageInput = document.querySelector('#image');
+    imageInput.addEventListener('change', () => {
+        document.querySelector('#previewImage').src = imageInput.value;
+    });
+});
