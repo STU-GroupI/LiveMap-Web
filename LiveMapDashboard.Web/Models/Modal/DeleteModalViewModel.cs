b9c0bc582eeec2012a7540fa2b1b045f46a47b0e
Luka <60887692+BotSquirrel@users.noreply.github.com>
2025-05-05 09:15:09 +0200

    Add "is used" check on deletion of categories and setup modernized delete modal component for future use (#96)
    * feat: add computed value to category and modernize delete modals
    
    * fix: remove itemgroup in csproj
    
    * fix: add newvalue to list

---
+++ LiveMapDashboard.Web/Models/Modal/DeleteModalViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace LiveMapDashboard.Web.Models.Modal;
+
+public record DeleteModalViewModel
+{
+    public string ModalId { get; set; } = Guid.NewGuid().ToString("N");
+    public string ModalTitle { get; set; } = string.Empty;
+
+    public string? WarningMessage { get; set; }
+    public string? DangerMessage { get; set; }
+
+    public string FormAction { get; set; } = string.Empty;
+    public string FormController { get; set; } = string.Empty;
+    public string FormMethod { get; set; } = "post";
+
+    public Dictionary<string, string> HiddenInputs { get; set; } = new();
+}
