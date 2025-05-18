92f78279f4f76c09f9d3d5549106e1363fcc1cbb
SuperGameLegend <e.sagir@student.avans.nl>
2025-05-18 20:59:02 +0200

    Quickfix!!!!!!

--- LiveMapDashboard.Web/Extensions/HttpContextExtensions.cs
+++ LiveMapDashboard.Web/Extensions/HttpContextExtensions.cs
@@ -5,13 +5,13 @@ public static class HttpContextExtensions
     private const string COOKIE_NAME = "SelectedMapId";
     public static Guid? GetCurrentMapId(this HttpContext context)
     {
-        if (context.Request.Cookies.TryGetValue(COOKIE_NAME, out var value) && Guid.TryParse(value, out var cookieId))
-        {
-            return cookieId;
-        } 
-        else if (context.Items.TryGetValue(COOKIE_NAME, out object? obj) && Guid.TryParse(obj as string, out var itemsId))
+        if (context.Items.TryGetValue(COOKIE_NAME, out object? obj) && Guid.TryParse(obj as string, out var itemsId))
         {
             return itemsId;
+        }
+        else if (context.Request.Cookies.TryGetValue(COOKIE_NAME, out var value) && Guid.TryParse(value, out var cookieId))
+        {
+            return cookieId;
         }
 
         return null;
