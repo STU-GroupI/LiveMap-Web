2d38ffd94805efe4608a4e696ee69028dce98250
SuperGameLegend <e.sagir@student.avans.nl>
2025-05-18 21:00:02 +0200

    I'm a genius

--- LiveMapDashboard.Web/Extensions/HttpContextExtensions.cs
+++ LiveMapDashboard.Web/Extensions/HttpContextExtensions.cs
@@ -5,6 +5,8 @@ public static class HttpContextExtensions
     private const string COOKIE_NAME = "SelectedMapId";
     public static Guid? GetCurrentMapId(this HttpContext context)
     {
+        /*I have switched these statements as the second statement could only be applicable in the next request, explaining why the mapId
+         * would never update instantly and refreshes were needed*/
         if (context.Items.TryGetValue(COOKIE_NAME, out object? obj) && Guid.TryParse(obj as string, out var itemsId))
         {
             return itemsId;
