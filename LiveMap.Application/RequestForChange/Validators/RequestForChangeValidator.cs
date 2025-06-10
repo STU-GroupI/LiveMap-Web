5335039bf20c2534df7965b89ffc969701236e74
JoshavEngelen <josha@mixnmac.nl>
2025-06-10 22:12:28 +0200

    fix: Added validationmessage for test

--- LiveMap.Application/RequestForChange/Validators/RequestForChangeValidator.cs
+++ LiveMap.Application/RequestForChange/Validators/RequestForChangeValidator.cs
@@ -10,5 +10,7 @@ public class RequestForChangeValidator : AbstractValidator<RequestForChange>
     public RequestForChangeValidator()
     {
         RuleFor(rfc => rfc.SuggestedPoi!).SetValidator(new SuggestedPointOfInterestValidator()).When(rfc => rfc.SuggestedPoi != null);
+        RuleFor(rfc => rfc.Poi!.Title).MaximumLength(30)
+            .WithMessage("Title should not be longer than 30 characters.").When(rfc => rfc.Poi != null);
     }
 }
