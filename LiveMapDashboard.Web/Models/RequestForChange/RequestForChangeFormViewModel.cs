da4606aac533a9c58b751fd2bf63f486a7f95e1a
JoshavEngelen <josha@mixnmac.nl>
2025-06-10 21:55:48 +0200

    fix: RFC validation messages working

--- LiveMapDashboard.Web/Models/RequestForChange/RequestForChangeFormViewModel.cs
+++ LiveMapDashboard.Web/Models/RequestForChange/RequestForChangeFormViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using LiveMap.Domain.Models;
 using LiveMapDashboard.Web.Models.Poi;
 using System.ComponentModel.DataAnnotations;
+using static NetTopologySuite.Geometries.Utilities.GeometryMapper;
+using System.Text.RegularExpressions;
 
 namespace LiveMapDashboard.Web.Models.Rfc;
 
@@ -11,6 +13,41 @@ public record RequestForChangeFormViewModel(
 {
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
-        return [];
+        var results = new List<ValidationResult>();
+
+        if (string.IsNullOrWhiteSpace(CrudformViewModel.Title))
+        {
+            results.Add(new ValidationResult("The Title field is required", new[] { nameof(CrudformViewModel.Title) }));
+        }
+
+        if (string.IsNullOrWhiteSpace(CrudformViewModel.MapId) || !Guid.TryParse(CrudformViewModel.MapId, out _))
+        {
+            results.Add(new ValidationResult("Park ID must be a valid GUID.", new[] { nameof(CrudformViewModel.MapId) }));
+        }
+
+        if (CrudformViewModel.Coordinate.Latitude == 0 && CrudformViewModel.Coordinate.Longitude == 0)
+        {
+            results.Add(new ValidationResult("A valid location must be added.", new[] { nameof(CrudformViewModel.Coordinate) }));
+        }
+
+        var openingsHours = CrudformViewModel.OpeningHours;
+        if (openingsHours != null)
+        {
+            foreach (var openingHour in openingsHours)
+            {
+                if (openingHour == null)
+                {
+                    results.Add(new ValidationResult("Each opening hour must be set.", new[] { nameof(openingsHours) }));
+                    continue;
+                }
+
+                if (!Regex.IsMatch(openingHour.Start, @"^[012345]\d\:[012345]\d$") || !Regex.IsMatch(openingHour.End, @"^[012345]\d\:[012345]\d$"))
+                {
+                    results.Add(new ValidationResult("Opening and closing hours must follow the format [HH:MM].", new[] { nameof(openingsHours) }));
+                }
+            }
+        }
+
+        return results;
     }
 }
